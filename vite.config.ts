b358849b7f4623f52205c91a0f9f8eb4966f1a20
Spearhead111 <18101524079@163.com>
2023-10-13 13:01:27 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        // 配置需要代理的路径 --> 这里的意思是代理http://localhost:80/api/后的所有路由
+        target: 'http://httpbin.org', // 目标地址 --> 服务器地址
+        changeOrigin: true // 允许跨域
+      }
+    }
+  }
+})
