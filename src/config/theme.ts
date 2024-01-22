b3313df223eaf2840e12456ddf2d105c6f2d5e92
Spearhead111 <18101524079@163.com>
2024-01-22 20:48:47 +0800

    ✨feat: 添加部分主题样式变量

--- src/config/theme.ts
+++ src/config/theme.ts
@@ -1,12 +1,15 @@
+// 这里配置的主题色必须在common.scss中注册一下
 const darkTheme = {
   '--hover-bg-color': '#494949',
   '--header-bg-color': 'rgb(120, 120, 120)',
-  '--second-bg-color': '#404040'
+  '--second-bg-color': '#404040',
+  '--third-bg-color': '#404040'
 }
 const lightTheme = {
   '--hover-bg-color': '#eeeeee',
   '--header-bg-color': 'rgb(80, 80, 80)',
-  '--second-bg-color': '#fbfbfb'
+  '--second-bg-color': '#fbfbfb',
+  '--third-bg-color': '#eee'
 }
 
 export const toggleThemeGlobal = (isDark: boolean) => {
