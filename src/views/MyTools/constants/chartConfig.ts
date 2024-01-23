2764dcd3d1bb5c7773cbda111680a103c3c003f2
Spearhead111 <18101524079@163.com>
2024-01-23 12:38:31 +0800

    ✨feat: 更改CHART_TYPE命名

--- src/views/MyTools/constants/chartConfig.ts
+++ src/views/MyTools/constants/chartConfig.ts
@@ -10,14 +10,22 @@ export const CHART_CONFIG_ITEMS = {
 }
 
 export const CHART_TYPES = {
+  LINE: 'Line',
+  LINE_DES: '折线图',
+  SCATTER: 'Scatter',
+  SCATTER_DES: '散点图'
+}
+
+/** 图表类型的icon */
+export const CHART_TYPES_ICON = {
   Line: {
-    name: 'Line',
-    desc: '折线图',
+    name: CHART_TYPES.LINE,
+    desc: CHART_TYPES.LINE_DES,
     icon: 'mdi-chart-line'
   },
   Scatter: {
-    name: 'Scatter',
-    desc: '散点图',
+    name: CHART_TYPES.SCATTER,
+    desc: CHART_TYPES.SCATTER_DES,
     icon: 'mdi-chart-scatter-plot'
   }
 }
